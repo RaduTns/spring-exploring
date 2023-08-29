7c5638ff37316d703b9b6a16b3348509ae882b7d
RaduTns <radu.tanase3@gmail.com>
2023-08-29 16:15:27 +0300

    Initial push, will work on the structure of the project later on

---
+++ inventory-service/src/main/java/com/personalproject/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,15 @@
+package com.personalproject.inventoryservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.personalproject.inventoryservice.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+
+	List<Inventory> findBySkuCodeIn(List<String> skuCode);
+
+}
